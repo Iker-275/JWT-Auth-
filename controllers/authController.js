ee9208d677c1bc7f26cb2736c124133e4165478b
Iker 275 <jeananderson275@gmail.com>
2025-11-06 23:07:24 +1000

    added validation to inputs on signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,20 +1,58 @@
+const User = require("../models/userModel");
 
 
+const handleErrors = (err) => {
+   // console.log(err.message,err.code);
 
-const signUp_get =  async(req,res)=>{
-res.render("signup");
+    let errors = { email: "", password: "" }
+
+     //duplicate error code
+    if(err.code === 11000){
+        errors.email ="That email is already registered";
+        return errors;
+    }
+
+
+    //validation errors
+    if (err.message.includes('user validation failed')) {
+        console.log(Object.values(err.errors));
+        Object.values(err.errors).forEach(
+            ({ properties }) => {
+                errors[properties.path] = properties.message;
+            }
+        )
+
+    }
+
+    return errors;
 }
 
-const signUp_post =  async(req,res)=>{
-res.render("new signup");
+const signUp_get = async (req, res) => {
+    res.render("signup");
 }
 
-const login_get =  async(req,res)=>{
-res.render('login');
+const signUp_post = async (req, res) => {
+    const { email, password } = req.body;
+    // console.log(email,password);
+
+    try {
+        const user = await User.create({ email, password });
+        //console.log(user);
+        res.status(201).json({ success: true, user });
+    } catch (error) {
+        const errors = handleErrors(error);
+        res.status(400).json({ success: false, errors })
+    }
 }
 
-const login_post =  async(req,res)=>{
-res.render('user login')
+const login_get = async (req, res) => {
+    res.render('login');
 }
 
-module.exports = {signUp_get,signUp_post,login_get,login_post};
+const login_post = async (req, res) => {
+    const { email, password } = req.body;
+    console.log(email, password);
+    res.send('user login')
+}
+
+module.exports = { signUp_get, signUp_post, login_get, login_post };
---
+++ models/userModel.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const { isEmail} = require('validator');
+
+const userSchema = new mongoose.Schema({
+    email:{
+        type:String,
+        required:[true,'Please provide an email'],
+        unique:true,
+        lowercase:true,
+        validate:[isEmail,'Please enter a valid email']
+    },
+    password:{
+        type:String,
+        required:[true,'Please provide a password'],
+        minlength:[6,'Password cannot be less than 6 characters']
+    }
+})
+
+
+const User = mongoose.model('user',userSchema);
+
+
+module.exports = User;
