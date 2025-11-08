6aed92203aff6c32c84d5828b9c1c2fad0f372fd
Iker 275 <jeananderson275@gmail.com>
2025-11-08 21:19:03 +1000

    password hashing and auth views added

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const { isEmail} = require('validator');
+const bcrypt = require ("bcrypt");
 
 const userSchema = new mongoose.Schema({
     email:{
@@ -17,7 +18,29 @@ const userSchema = new mongoose.Schema({
 })
 
 
+//before event is fired
+userSchema.pre('save',async function(next){
+const salt = await bcrypt.genSalt();
+this.password = await bcrypt.hash(this.password,salt);
+//console.log('user about to be created',this);
+next();
+})
+
+
 const User = mongoose.model('user',userSchema);
 
 
 module.exports = User;
+
+
+
+//mongoose hooks
+// userSchema.post('save',function(doc,next){
+// console.log('new user was created and saved',doc);
+// next();//after 
+// });
+// //before event is fired
+// userSchema.pre('save',(next)=>{
+// console.log('user about to be created',this);
+// next();
+// })
